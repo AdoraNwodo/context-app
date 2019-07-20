c4d0243f0d234e864de86184b1ac754496c0293e
nennenwodo <nennenwodo@gmail.com>
2019-07-20 04:58:51 +0100

    Add SeriesList class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
+import SeriesList from './components/SeriesList';
 
 const App = () => {
   return (
     <div>
       <Header />
-      <p>Hello world!</p>
+      <SeriesList />
     </div>
   )
 }
---
+++ src/components/SeriesList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class SeriesList extends Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li>How I met you mother</li>
+                    <li>Game of Thrones</li>
+                    <li>The Originals</li>
+                    <li>Power</li>
+                    <li>Hit the floor</li>
+                </ul>
+            </div>
+        )
+    }
+}
