b8eb6b1868118865781a296ddb46a9b6d44f00c2
nennenwodo <nennenwodo@gmail.com>
2019-07-20 05:04:16 +0100

    Add default styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <div>
+        <div className="header">
             <h1>SAMPLE HOOKS CONTEXT APP</h1>
         </div>
     )
--- src/components/SeriesList.js
+++ src/components/SeriesList.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class SeriesList extends Component {
     render() {
         return (
-            <div>
+            <div className="series-list">
                 <ul>
                     <li>How I met you mother</li>
                     <li>Game of Thrones</li>
